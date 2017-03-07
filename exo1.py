6b78a0a6d6dcc369038c42aa51510c813538069f
AdelineChn <adeline.chouin@hotmail.fr>
2017-03-07 09:45:54 +0100

    Update exo1.py

--- exo1.py
+++ exo1.py
@@ -1,17 +1,16 @@
 #! / usr / bin / env python3 
 import ev3dev.ev3 as ev3
 from ev3dev.ev3  import  *
+from time import sleep
+import sys
+coding : utf-8
 
-ev3.Sound.setLanguage("French") #définir la langue sur français
-
-
-cl = colorSENSOR () #définir le capteur comme capteur de couleur
+cl = colorSensor () #définir le capteur comme capteur de couleur
 assert cl.connected, "Connecter un capteur de couleur à un port de capteur" #vérifier que le capteur est connecté sinon erreur
 cl.mode = 'COL-COLOR' #mettre le capteur cl en mode reconnaissance de couleur
-colors = ( "inconnu", "noir", "bleu", "vert", "vert", "rouge", "blanc", "marron")
-if colors = "rouge":
-	ev3.Sound.speak('Au Revoir').exit() #si la couleur est rouge, le programme s'arrête
-	pass
-else :
-	print(colors[cl.value()]) #affiche la couleur en chaine de caractère
-    Sound.speak(colors[cl.value()]).wait() #dit la couleur
+colors = ( 'inconnu', 'noir', 'bleu', 'vert', 'jaune', 'Au Revoir !', 'blanc', 'marron')
+while cl.value() != 5:
+	Sound.speak(color[cl.value()]).wait() #tant que la couleur est différente de rouge, le programme dicte les couleurs qu'il voit
+	sleep(2)
+if cl.values() == 5:
+	Sound.speak(colors[cl.value()]).wait() #si la couleur est rouge, le programme s'arrête
